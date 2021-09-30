2af434f0ffdc7a7c042181e8e873c40b823514af
Van Huynh <vthuynh@cpp.edu>
2021-09-29 22:37:28 -0700

    create outline of RestaurantBill Program

---
+++ RestaurantBill.c
@@ -0,0 +1,8 @@
+/* RestaurantBill.c*/
+/*
+Restaurant Bill:
+1. Ask user to input tax and tip percentage
+2. Choose a random meal out of 4 and calculate 
+    the tax and tip based on the meal's price
+3. Display the meal cost, tax amount, tip amount, and total bill 
+*/
